e30d630c9e1d37194823671a98e47c072a0799af
bzsmbr <119484999+bzsmbr@users.noreply.github.com>
2025-03-25 14:22:08 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+const movies = [
+    {id: "1", title: "movie1", director: "director1", releaseYear: "2000", wonOscar: "yes"},
+    {id: "2",title: "movie2", director: "director2", releaseYear: "2001", wonOscar: "no"},
+    {id: "3",title: "movie3", director: "director3", releaseYear: "2002", wonOscar: "yes"}
+]
+export default movies;
